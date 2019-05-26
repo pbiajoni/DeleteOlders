993c94797e95334f9e510e671354c7dccc5e31d2
Pablo Biajoni <pbiajoni@hotmail.com>
2019-05-26 13:05:03 -0300

    Algumas alterações

--- DeleteOlders/Program.cs
+++ DeleteOlders/Program.cs
@@ -8,10 +8,16 @@ namespace DeleteOlders
 {
     class Program
     {
+        static string log = "";
 
+        static void Log(string text)
+        {
+            log += DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " - " + text + Environment.NewLine;
+            Console.WriteLine(text);
+        }
         static string GetArg(string param, string[] args)
         {
-            foreach(string arg in args)
+            foreach (string arg in args)
             {
                 if (arg.Contains(param))
                 {
@@ -49,6 +55,65 @@ namespace DeleteOlders
 
             string rootDirectory = GetArg("-d", args);
             DirectoryInfo directoryInfo = new DirectoryInfo(rootDirectory);
+
+            int thisMonth = DateTime.Now.Month;
+            int max = GetArg("-max", args) == null ? -1 : Convert.ToInt32(GetArg("-max", args));
+            int oldMax = GetArg("-oldmax", args) == null ? -1 : Convert.ToInt32(GetArg("-oldmax", args));
+
+            if (max != -1)
+            {
+                Log("Excluindo diretórios de " + DateTime.Now.Month + "/" + DateTime.Now.Year + " mantendo último(s) " + max + " dia(s)");
+                List<DirectoryInfo> monthDirectories = directoryInfo.GetDirectories()
+                    .Where(x => (x.CreationTime.Month == DateTime.Now.Month) && (x.CreationTime.Year == DateTime.Now.Year)).ToList();
+
+                int foldersCount = monthDirectories.Count;
+                int firstCount = foldersCount - max;
+
+                if (foldersCount > max)
+                {
+                    List<DirectoryInfo> foldersToDelete = monthDirectories.OrderBy(x => x.CreationTime).Take(firstCount).ToList();
+
+                    foreach (DirectoryInfo dirToDelete in foldersToDelete)
+                    {
+                        Log("Excluindo diretório:" + dirToDelete.FullName);
+                        //Directory.Delete(dirToDelete.FullName);
+                    }
+
+                }
+            }
+
+            List<DirectoryInfo> oldMonthDirectories = directoryInfo.GetDirectories()
+                    .Where(x => (x.CreationTime.Month != DateTime.Now.Month && x.CreationTime.Year != DateTime.Now.Year)).ToList();
+
+            int oldFoldersCount = oldMonthDirectories.Count;
+            if (ArgExists("-oldmax", args))
+            {                
+                int oldFirstCount = oldFoldersCount - oldMax;
+
+                if (oldFoldersCount > oldMax)
+                {
+                    Log("Excluindo meses anteriores mantendo último(s) " + oldMax + " dia(s) de cada mês");
+                    List<DirectoryInfo> oldFoldersToDelete = oldMonthDirectories.OrderBy(x => x.CreationTime).Take(oldFirstCount).ToList();
+
+                    foreach (DirectoryInfo oldDirToDelete in oldFoldersToDelete)
+                    {
+                        Log("Excluindo diretório antigo:" + oldDirToDelete.Name);
+                        //Directory.Delete(dirToDelete.FullName);
+                    }
+
+                }
+            }
+            else
+            {
+                if (oldFoldersCount > 0)
+                {
+                    Log("Excluindo todos os meses mais antigos");
+                    foreach (DirectoryInfo allOldDirectoriesToDelete in oldMonthDirectories)
+                    {
+                        Log("Excluindo todos diretórios antigos:" + allOldDirectoriesToDelete.Name);
+                    }
+                }
+            }
 
         }
 
