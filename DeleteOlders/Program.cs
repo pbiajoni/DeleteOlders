f0db6a18e27ddbd6c74ae79df00cf497b7639be0
Pablo Biajoni <pbiajoni@hotmail.com>
2019-05-26 10:52:43 -0300

    Adicionado extensions

--- DeleteOlders/Program.cs
+++ DeleteOlders/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -7,9 +8,49 @@ namespace DeleteOlders
 {
     class Program
     {
+
+        static string GetArg(string param, string[] args)
+        {
+            foreach(string arg in args)
+            {
+                if (arg.Contains(param))
+                {
+                    return arg.Replace(param, "");
+                }
+            }
+
+            return null;
+        }
+
+        static bool ArgExists(string param, string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Contains(param))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                throw new Exception("Nenhum argumento encontrado");
+            }
+
+            if (GetArg("-d", args) == null)
+            {
+                throw new Exception("Requer o argumento -d com o diretório a ser verificado");
+            }
+
+            string rootDirectory = GetArg("-d", args);
+            DirectoryInfo directoryInfo = new DirectoryInfo(rootDirectory);
 
         }
+
     }
 }
